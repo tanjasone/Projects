013503dc4e7c6b0c835b0626a5ad4f5bd4bce699
tanjasone <40807146+tanjasone@users.noreply.github.com>
2020-03-18 21:36:53 -0400

    Added queen exchange

--- chess/GameController.java
+++ chess/GameController.java
@@ -1,12 +1,13 @@
-package chess;
-
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GameController implements MouseListener{
 	
@@ -23,7 +24,6 @@ public class GameController implements MouseListener{
 	}
 
 	public void setTileArray(BoardTile[][] t){
-
 	    tiles = t;
 	}
 
@@ -39,7 +39,8 @@ public class GameController implements MouseListener{
         if(currentSelect == null && b.cp == null) return;
 
         if(currentSelect != null && b.cp != null && !b.equals(currentSelect) &&
-		b.cp.color.equals(currentSelect.cp.color)){
+        	b.cp.color.equals(currentSelect.cp.color))
+        {
             System.out.println("Resetting");
         	currentSelect.setBorder(new EmptyBorder(0,0,0,0));
             for(int i=0; i<moves.size(); i++){
@@ -67,7 +68,7 @@ public class GameController implements MouseListener{
 				}
 			}
 			turnPhase = 1;
-			System.out.println("----------------");
+			System.out.println();
 			return;
 		}
 		else if(turnPhase==1){
@@ -75,9 +76,20 @@ public class GameController implements MouseListener{
             if(!moves.contains(b)) return;
 
             System.out.println("Turn phase 1");
-
+            boolean pawnChange = false;
+            Label oldLabel=null;
+            
             if(b.cp!=null)
 				b.remove(b.cp.name);
+            if(currentSelect.cp instanceof Pawn)
+            {
+            	oldLabel = currentSelect.cp.name;
+            	pawnChange = true;
+            	if(currentSelect.cp.color.equals(Color.BLACK) && b.position.y == 7)
+            		currentSelect.cp = convertPawn(currentSelect.cp.color);
+            	if(currentSelect.cp.color.equals(Color.WHITE) && b.position.y == 0)
+            		currentSelect.cp = convertPawn(currentSelect.cp.color);
+            }
 			b.cp = currentSelect.cp;
 			b.add(b.cp.name);
 			b.cp.pos = b.position;
@@ -87,8 +99,11 @@ public class GameController implements MouseListener{
 			for(int i=0; i<moves.size(); i++){
 				moves.get(i).setBorder(new EmptyBorder(0,0,0,0));
 			}
-
-			currentSelect.remove(currentSelect.cp.name);
+			
+			if(pawnChange)
+				currentSelect.remove(oldLabel);
+			else
+				currentSelect.remove(currentSelect.cp.name);
 			currentSelect.cp = null;
 			currentSelect = null;
 			if(b.cp instanceof Pawn && ((Pawn) b.cp).firstTurn){
@@ -99,6 +114,31 @@ public class GameController implements MouseListener{
             turns++;
 		}
 		System.out.println("----------------");
+	}
+
+	private ChessPiece convertPawn(Color color) 
+	{
+		String[] selectionValues = {"Queen", "Bishop", "Rook", "Knight"};
+		ChessPiece cp = null;
+		String choice;
+		
+		while(cp == null) 
+		{
+			choice = (String) JOptionPane.showInputDialog(null, "What type of piece do want to exchange the Pawn with?", null, 
+				JOptionPane.QUESTION_MESSAGE, null, selectionValues, "[select]");
+			if(choice.toUpperCase().equals("QUEEN"))
+				cp = new Queen(color);
+			else if(choice.toUpperCase().equals("BISHOP"))
+				cp = new Bishop(color);
+			else if(choice.toUpperCase().equals("ROOK"))
+				cp = new Rook(color);
+			else if(choice.toUpperCase().equals("KNIGHT"))
+				cp = new Knight(color);
+			else
+				JOptionPane.showInternalMessageDialog(null, "That is not a valid option", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		return cp;
 	}
 
 //*****************************************************
